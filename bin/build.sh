7dfeffcdc25168eca9a32d468a550b8bfe26a53a
Eddie-boi <edvard.thornros@gmail.com>
2017-04-23 18:35:54 +0200

    IDK

--- bin/build.sh
+++ bin/build.sh
@@ -5,7 +5,7 @@ echo ""
 echo "---------- CMAKE ---------"
 export CC=clang
 export CXX=clang++
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake cmake -DCMAKE_BUILD_TYPE=Debug ..
 
 if [ $? -ne 0 ]; then
 	echo "Failed to run Cmake"
