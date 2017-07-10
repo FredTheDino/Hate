1112569ac4d3416be5b115d3f17a815f50c483de
ed <ed@fake.com>
2017-07-10 23:23:45 +0200

    Transform unions don't work... It bugs me...

--- src/game/playing_state.cpp
+++ src/game/playing_state.cpp
@@ -7,7 +7,23 @@
 namespace ps {
 	using namespace hate;
 
-	Transform player;
+	struct Player {
+		union {
+			Transform t;
+			struct {
+				Vec2 position;
+				Vec2 scale;
+				float rotation;
+			};
+		};
+
+		Vec2 vel;
+
+		bool grounded = false;
+
+	} player;
+
+	float gravity = 6.0f;
 
 	void playing_load() {
 		draw_color(Vec4(0.75, 0.22, 0.80, 1.0));
@@ -18,13 +34,20 @@ namespace ps {
 	}
 
 	void playing_update(float delta) {
-		if (is_down("down")) {
-			player.position.y += delta;
+
+		player.vel.y += gravity * delta;
+		player.grounded = false;
+		if (player.position.y >= 0.0f) {
+			player.position.y = 0.0f;
+			player.vel.y = 0.0f;
+			player.grounded = true;
 		}
 
-		if (is_down("up")) {
-			player.position.y -= delta;
+		if (player.grounded && is_pressed("up")) {
+			player.vel.y = -3.0f;
 		}
+
+		player.position.y += player.vel.y * delta;
 	}
 
 	void playing_draw() {
